e97f831fa933ab2c68c0680d671f81769e99ef62
ArGul <argul.mail@gmail.com>
2025-05-06 11:11:16 +0300

    Добавьте файлы проекта.

---
+++ ArGulOS/Kernel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace ArGulOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        private static string OSName = "ArGulOS";
+        private static string OSVersion = "Alpha 0.1";
+        private static string Separator25 = "=========================";
+        protected override void BeforeRun()
+        {
+            //Перед запуском
+            Console.Clear();
+            Console.WriteLine($"Welcome to {OSName}. Version: {OSVersion}");
+        }
+
+        protected override void Run()
+        {
+            //После запуска
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{OSName} >>: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            var input = Console.ReadLine();
+            switch ( input )
+            {
+                case "help":
+                    PrintAllComands();
+                    break;
+                case "clear":
+                    Console.Clear();
+                    break;
+                case "shutdown":
+                   Cosmos.System.Power.Shutdown();
+                    break;
+                case "reboot":
+                    Cosmos.System.Power.Reboot();
+                    break;
+                case "sysinfo":
+                    //Получаем данные о железе (Не всё, но часть)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    string CPUBrand = Cosmos.Core.CPU.GetCPUBrandString();
+                    string CPUVendor = Cosmos.Core.CPU.GetCPUVendorName();
+                    uint AmoutOfRAM = Cosmos.Core.CPU.GetAmountOfRAM();
+                    ulong AvailableRAM = Cosmos.Core.GCImplementation.GetAvailableRAM();
+                    uint UsedRAM = Cosmos.Core.GCImplementation.GetUsedRAM();
+
+                    Console.WriteLine(Separator25);
+                    Console.WriteLine("CPU: {0}\nCPU Vendor: {1}\nAmount of RAM: {2} MB\nAvailable RAM: {3} MB\nUsed RAM: {4} Byte\n", CPUBrand, CPUVendor, AmoutOfRAM, AvailableRAM, UsedRAM);
+                    Console.WriteLine(Separator25);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{input}: Command is not found");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+        private void PrintAllComands()
+        {
+            Console.WriteLine("This all commands:\n");
+            Console.WriteLine("clear: clear all terminal");
+            Console.WriteLine("shutdown: turning off the PC");
+            Console.WriteLine("reboot: rebooting the PC");
+            Console.WriteLine("sysinfo: information about the system");
+
+            Console.WriteLine();
+        }
+    }
+}
